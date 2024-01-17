75d5a0f7f2fd52d8a69defff82aa239301cb99c6
Tiberius <35663392+TIberiu99p@users.noreply.github.com>
2024-01-17 12:01:16 +0000

    Add files via upload

---
+++ TestSorting/SortingAlgorithms.Test/TestSortingAlgorithms.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace SortingAlgorithms.Test
+{
+    [TestFixture]
+    public class TestSortingAlgorithms
+    {
+        public int[] arr;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Shared array for all test methods
+            arr = new int[] { 12, 11, 13, 5, 6, 7 };
+        }
+
+        [Test]
+        public void MergeSort_SortsIntArray()
+        {
+            // Act
+            MergeSort.MergeSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.EqualTo(new int[] { 5, 6, 7, 11, 12, 13 }));
+        }
+
+        [Test]
+        public void MergeSort_SortsEmptyArray()
+        {
+            // Arrange
+            arr = new int[0];
+
+            // Act
+            MergeSort.MergeSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.Empty);
+        }
+
+        [Test]
+        public void QuickSort_SortsIntArray()
+        {
+            // Act
+            QuickSort.QuickSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.EqualTo(new int[] { 5, 6, 7, 11, 12, 13 }));
+        }
+
+        [Test]
+        public void QuickSort_SortsEmptyArray()
+        {
+            // Arrange
+            arr = new int[0];
+
+            // Act
+            QuickSort.QuickSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.Empty);
+        }
+
+        [Test]
+        public void SelectionSort_SortsIntArray()
+        {
+            // Act
+            SelectionSort.SelectionSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.EqualTo(new int[] { 5, 6, 7, 11, 12, 13 }));
+        }
+
+        [Test]
+        public void SelectionSort_SortsEmptyArray()
+        {
+            // Arrange
+            arr = new int[0];
+
+            // Act
+            SelectionSort.SelectionSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.Empty);
+        }
+
+        [Test]
+        public void ShellSort_SortsIntArray()
+        {
+            // Act
+            ShellSort.ShellSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.EqualTo(new int[] { 5, 6, 7, 11, 12, 13 }));
+        }
+
+        [Test]
+        public void ShellSort_SortsEmptyArray()
+        {
+            // Arrange
+            arr = new int[0];
+
+            // Act
+            ShellSort.ShellSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.Empty);
+        }
+
+        [Test]
+        public void InsertionSort_SortsIntArray()
+        {
+            // Act
+            InsertionSort.InsertionSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.EqualTo(new int[] { 5, 6, 7, 11, 12, 13 }));
+        }
+
+        [Test]
+        public void InsertionSort_SortsEmptyArray()
+        {
+            // Arrange
+            arr = new int[0];
+
+            // Act
+            InsertionSort.InsertionSortAlgorithm(arr);
+
+            // Assert
+            Assert.That(arr, Is.Empty);
+        }
+
+
+
+
+    }
+}
